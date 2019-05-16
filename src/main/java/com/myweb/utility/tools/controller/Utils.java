0d6f525374b8d8f12dd2b45ffafd1b87e7510bbb
Jegatheesh <jegatheesh@medassolutions.com>
2019-05-16 16:37:56 +0400

    Util function added -> To convert webpage to html

--- src/main/java/com/myweb/utility/tools/controller/Utils.java
+++ src/main/java/com/myweb/utility/tools/controller/Utils.java
@@ -1,5 +1,14 @@
 package com.myweb.utility.tools.controller;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.springframework.util.StringUtils;
 
 /**
@@ -62,5 +71,44 @@ public class Utils {
 			logBuilder.append(logContent + lineSeperator);
 		}
 		System.out.println(logContent);
+	}
+	
+	public static void convertWebpage2HTML(String domain, String url, String fileName) {
+		InputStream is = null;
+		BufferedReader br = null;
+		BufferedWriter writer = null;
+		try {
+			URL uri = new URL(domain + url);
+			is = uri.openStream(); // throws an IOException
+			br = new BufferedReader(new InputStreamReader(is));
+			writer = new BufferedWriter(new FileWriter(fileName + ".html"));
+			StringBuilder sb = new StringBuilder();
+			try {
+				br.lines().forEach(line -> {
+					line = line.replaceAll("src=\"", "src=\"" + domain);
+					sb.append(line + "\n");
+				});
+			} catch (Exception e) {
+
+			}
+			writer.append(sb);
+			writer.close();
+		} catch (MalformedURLException mue) {
+			mue.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally {
+			try {
+				if (is != null)
+					is.close();
+			} catch (IOException ioe) {
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		convertWebpage2HTML("http://192.168.0.30:8080", 
+				"/eclinic/dentalConsultAction.do?method=printDentalChart&op_number=KNG-03-000001&consult_id=11&to_date=16-05-2019", 
+				"data");
 	}
 }
