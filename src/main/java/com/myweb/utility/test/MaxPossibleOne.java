6694eb6f9935af190be05465496c48e6de3e3333
Jegatheesh <jegatheesh93@gmail.com>
2019-08-09 11:16:29 +0400

    Fix:  - Max possible ones

--- src/main/java/com/myweb/utility/test/MaxPossibleOne.java
+++ src/main/java/com/myweb/utility/test/MaxPossibleOne.java
@@ -11,9 +11,90 @@ public class MaxPossibleOne {
 		findPossibleOnes(5, new int[] { 1, 0, 1, 1, 1 });
 		findPossibleOnes(3, new int[] { 1, 1, 1 });
 		findPossibleOnes(8, new int[] { 0, 0, 1, 1, 1, 0, 1, 0 });
+		findPossibleOnes(8, new int[] { 0, 0, 1, 1, 1, 0, 1, 1 });
 	}
 
 	public static int findPossibleOnes(int size, int[] arr) {
+		int prev = -1;
+		int current = 0, max = 0;
+		int pause = 0;
+		int ones = 0, lastOnes = 0;
+		int maxAt = -1;
+		for (int i = 0; i < arr.length; i++) {
+			// If first value is zero
+			if (prev == -1 && arr[i] == 0) {
+				pause++;
+			}
+			if (prev != arr[i]) {
+				pause++;
+			}
+			if (arr[i] == 0) {
+				lastOnes = ones;
+				ones = 0;
+			} else {
+				ones++;
+			}
+			if (pause > 3) {
+				if (current > max) {
+					max = current;
+					maxAt = i;
+				}
+				pause = 2;
+				current = lastOnes;
+			}
+			current++;
+			prev = arr[i];
+		}
+		// cases
+		// Ends at 0
+		// No zeros
+		// Three pauses and ends out of loop
+		if (arr[arr.length - 1] == 0 || arr[arr.length - 1] == 1 && pause == 1 || pause == 3) {
+			if (current > max) {
+				max = current;
+				maxAt = -1;
+			}
+		}
+		System.out.println("Get max at " + (maxAt == -1 ? "end" : maxAt) + " - > " + max);
+		return max;
+	}
+
+	// Fail Attempts
+	public static int findPossibleOnesF1(int size, int[] arr) {
+		int maxBound = 0;
+		int start = -1;
+		int end = -1;
+		int ones = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 0) {
+				end++;
+				if (start == -1)
+					start++;
+			} else {
+				ones++;
+				if (end != -1) {
+					int diff = end - start + 1;
+					if (diff > maxBound) {
+						maxBound = diff;
+					}
+					start = -1;
+					end = -1;
+				}
+			}
+		}
+		if (end != -1) {
+			int diff = end - start + 1;
+			if (diff > maxBound) {
+				maxBound = diff;
+			}
+			start = -1;
+			end = -1;
+		}
+		System.out.println(ones + maxBound);
+		return ones + maxBound;
+	}
+	
+	public static int findPossibleOnesF2(int size, int[] arr) {
 		int maxBound = 0;
 		int start = -1;
 		int end = -1;
@@ -74,41 +155,6 @@ public class MaxPossibleOne {
 			}
 		}
 		System.out.print(max[0] + ", " + max[1] + " -> ");
-		System.out.println(ones + maxBound);
-		return ones + maxBound;
-	}
-
-	// Fail Attempts
-	public static int findPossibleOnesF1(int size, int[] arr) {
-		int maxBound = 0;
-		int start = -1;
-		int end = -1;
-		int ones = 0;
-		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] == 0) {
-				end++;
-				if (start == -1)
-					start++;
-			} else {
-				ones++;
-				if (end != -1) {
-					int diff = end - start + 1;
-					if (diff > maxBound) {
-						maxBound = diff;
-					}
-					start = -1;
-					end = -1;
-				}
-			}
-		}
-		if (end != -1) {
-			int diff = end - start + 1;
-			if (diff > maxBound) {
-				maxBound = diff;
-			}
-			start = -1;
-			end = -1;
-		}
 		System.out.println(ones + maxBound);
 		return ones + maxBound;
 	}
