676fb6af7246cdac529a3bb71c3832762e93798b
Jegatheesh <jegatheesh93@gmail.com>
2019-08-16 13:13:38 +0400

    Add2ListNode -> Improved version

--- src/main/java/com/myweb/utility/test/problems/Add2ListNode.java
+++ src/main/java/com/myweb/utility/test/problems/Add2ListNode.java
@@ -28,8 +28,84 @@ public class Add2ListNode {
 			return val + " -> " + next;
 		}
 	}
-
+	
+	/**
+	 * Improved version
+	 * 
+	 * Runtime: 2 ms	Memory: 45.2 MB
+	 * @param l1
+	 * @param l2
+	 * @return
+	 */
 	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode sum = new ListNode(0);
+		ListNode prev = sum, p1 = l1, p2 = l2;
+		int carry = 0;
+		while (p1 != null || p2 != null || carry > 0) {
+			// System.out.print("In: " + (p1 != null ? p1.val : -1) + "&" + (p2 != null ? p2.val : -1) + " Carry: " + carry);
+			if (p2 != null) {
+				carry += p2.val;
+				p2 = p2.next;
+			}
+			if (p1 != null) {
+				carry += p1.val;
+				p1 = p1.next;
+			}
+			prev.val = carry % 10;
+			carry = carry / 10;
+			System.out.println(" Out: " + prev.val);
+			if (p1 != null || p2 != null || carry > 0) {
+				prev.next = new ListNode(0);
+				prev = prev.next;
+			}
+		}
+		return sum;
+	
+	}
+	
+	/**
+	 * Runtime Improved version
+	 * 
+	 * Runtime: 46 ms	Memory: 46.8 MB
+	 * @param l1
+	 * @param l2
+	 * @return
+	 */
+	public ListNode addTwoNumbersA2(ListNode l1, ListNode l2) {
+		ListNode sum = new ListNode(0);
+		ListNode prev = sum, p1 = l1, p2 = l2;
+		int carry = 0;
+		int tot = 0;
+		while (p1 != null || p2 != null || carry > 0) {
+			// System.out.print("In: " + (p1 != null ? p1.val : -1) + "&" + (p2 != null ? p2.val : -1) + " Carry: " + carry);
+			if (p2 != null) {
+				tot += p2.val;
+				p2 = p2.next;
+			}
+			if (p1 != null) {
+				tot += p1.val;
+				p1 = p1.next;
+			}
+			carry = tot / 10;
+			prev.val = tot % 10;
+			System.out.println(" Out: " + prev.val);
+			if (p1 != null || p2 != null || carry > 0) {
+				prev.next = new ListNode(0);
+				prev = prev.next;
+				tot = carry;
+			}
+		}
+		return sum;
+	}
+
+	/**
+	 * 
+	 * Runtime: 48 ms	Memory: 46.2 MB
+	 * @param l1
+	 * @param l2
+	 * @return
+	 */
+	public ListNode addTwoNumbersA1(ListNode l1, ListNode l2) {
 		ListNode sum = new ListNode(0);
 		ListNode prev = sum, p1 = l1, p2 = l2;
 		int carry = 0;
